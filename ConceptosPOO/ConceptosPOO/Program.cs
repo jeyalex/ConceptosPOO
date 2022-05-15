c8eb0ac1fbe651bc4f125b38e97a9fb55d23e4b1
jeison sandoval <jeissonsandoval1993@gmail.com>
2022-05-15 15:34:58 -0500

    Validacion de fechas en c#

---
+++ ConceptosPOO/ConceptosPOO/Program.cs
@@ -0,0 +1,45 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+
+
+using ConceptosPOO;
+
+Console.WriteLine("DE TU MANO TODO LO PUEDO tobares");
+
+//instanciar un objeto de la clase year
+//clDate fecha1 = new clDate(2022, 2, 11);// es mejor tipar el objeto
+
+try
+{
+    Console.WriteLine(new clDate(2022, 2, 11));// asi no la ejecuta x q la clase date hereda d la object hay q sobreescribir el metodo q a la final se quita el tostring
+    Console.WriteLine(new clDate(2022, 8, 30));
+    Console.WriteLine(new clDate(2022, 11, 15));
+}
+
+    catch (Exception error)              //la clese exepction ereda de MonthException
+{
+
+    Console.WriteLine(error.Message);               
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ConceptosPOO/ConceptosPOO/clDate.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceptosPOO
+{
+    public class clDate
+    {
+        //definimos los atributos
+        private int _year;
+        private int _month;
+        private int _day;
+
+
+        // creamos el constructor
+        public clDate(int year, int month, int day) //le pasamos los parametros al constructor
+        {
+            _year = year;
+            _month = CheckMonth(month);            //creams el metodo checkmonth
+            _day = CheckDay(day, month, day);
+
+        }
+
+        private int CheckDay(int year, int month, int day)      //para validar el di se deben pasar los 3parametros x febrero y año bisiesto
+        {
+            if (month == 2 && day == 29 && biciesto(year))     //aqui se genera el metodo año biciesto
+            {
+                return day;
+
+            }
+
+            //  creamos un arrego x almacenar os dias por mes
+            int[]diasPorMes = {0,31,28,31,30,31,30,31,31,30,31,30,31};
+            if (day >=1 && day <= diasPorMes[month])           //
+            {
+                return day;
+
+            }
+
+            throw new DayException("--DIA INVALIDO--");
+        }
+
+
+
+        private bool biciesto(int year)
+        {
+            return year % 400 == 0 || year % 4 == 0 && year % 100 != 0;                  //si esmodulo de 400 ya es biciesto para todos tanto para 100 y 4
+                                                                                            //esto evito miles de IF
+        }
+
+        private int CheckMonth(int month)             // validamos si un fecha MES es valido
+        {
+            if (month >= 1 && month <= 12)
+            {
+                return month;
+            }                                           // mi clase date no debe intervenir con el usuario x eso no se coloca el console.write
+
+
+            throw new MonthException("MES INVALIDO");      //SIGUE CON ERROR GENERAMOS LA CLASE           //creamos nuestra propia ecepcion
+                                                           // AL ejecutar se genera error y x eso es mejor capturar el errror
+        }
+
+        public override string ToString()                 // con espacio para generar l metodo
+        {
+            return $"{_year }/ { _month:00}/{_day:00}";       //interpolacion siempre con $
+        }
+
+
+
+    }
+}
